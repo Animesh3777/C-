555f2b09fed6919e2b7462cf966041ab12ee9cf1
Animesh Kumar <animesh.ak19@gmail.com>
2023-12-25 13:53:51 +0530

    Update 1st_programme.cpp

--- 1st_programme.cpp
+++ 1st_programme.cpp
@@ -5,11 +5,18 @@ int main(){
     // cout<<"Namaste Bharat!"<<endl;
     // cout<<"\n";
 
-    cout<<"Enter a number:" <<endl;
+    // cout<<"Enter a number:" <<endl;
 
-    int a;
+    // int a;
     // taking input
-    cin>>a;
+    // cin>>a;
 
-    cout<<"You entered " <<a<<endl;
+    // cout<<"You entered " <<a<<endl;
+
+    int a=5;
+    cout << "Value of a is : " << a << endl;
+
+    char ch = 'a';
+    cout<<"Value of Char is : "<<ch<<endl;
+
 }
