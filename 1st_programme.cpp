05ca540b3313e0720f40a05be205625a61a57f28
Animesh Kumar <animesh.ak19@gmail.com>
2023-12-25 12:50:07 +0530

    Update 1st_programme.cpp

--- 1st_programme.cpp
+++ 1st_programme.cpp
@@ -2,5 +2,14 @@
 using namespace std;
 
 int main(){
-    cout<<"Namaste Bharat!\n";
+    // cout<<"Namaste Bharat!"<<endl;
+    // cout<<"\n";
+
+    cout<<"Enter a number:" <<endl;
+
+    int a;
+    // taking input
+    cin>>a;
+
+    cout<<"You entered " <<a<<endl;
 }
