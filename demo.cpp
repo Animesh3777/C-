0b8c7d3e907c9bc049091e465de4a2c2cf365b7e
Animesh Kumar <animesh.ak19@gmail.com>
2023-12-28 22:53:36 +0530

    Update demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 using namespace std;
-int main(){
-    cout<<"Hello World"<<endl;
+
+int main() {
+    int marks;
+    cin >> marks;
+
+    if (marks >= 90) {
+        cout << "A Grade" << endl;
+    } else if (marks >= 80) {
+        cout << "B Grade" << endl;
+    } else if (marks >= 60) {
+        cout << "C Grade" << endl;
+    } else if (marks >= 40) {
+        cout << "D Grade" << endl;
+    } else {
+        cout << "Fail" << endl;
+    }
+
+    return 0;
 }
