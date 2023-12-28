a296f4b3ce69c6266c30667bcb40d240ef1a0241
Animesh Kumar <animesh.ak19@gmail.com>
2023-12-28 21:09:56 +0530

    Update 1st_programme.cpp

--- 1st_programme.cpp
+++ 1st_programme.cpp
@@ -1,22 +1,76 @@
 #include <iostream>
 using namespace std;
+// int main(){
+//     cout<<"Namaste Bharat"<<endl;
+//     cout<<'\n';
+// }
+
+// int main(){
+//     cout<<"Enter a number"<<endl;
+//     int a;
+//     // taking input
+//     cin>>a;
+//     cout<<"The number is "<<a<<endl;
+// }
+
+// int main(){
+//     int a=5;
+//     cout<<"Value of a is :"<<a<<endl;
+//     }
+
+// int main(){
+//     char ch='a';
+//     cout<<"Value of ch is :"<<ch<<endl;
+
+// } 
+
+// int main(){
+//     bool flag=true;
+//     cout<<"Value of true Boolean is :"<<flag<<endl;
+// }
+
+// int main(){
+//     int a=5;
+//     cout<<sizeof(a)<<endl;
+// }
+
+// int main(){
+//     char a=97;
+//     cout<<a<<endl;
+
+//     int b='b';
+//     cout<<b<<endl;
+
+// }
+
+// int main(){
+//     double d=5.7;
+
+//     int x =(int)d+5;
+//     cout<<"Value of x is : "<<x<<endl;
+// }
 
 int main(){
-    // cout<<"Namaste Bharat!"<<endl;
-    // cout<<"\n";
-
-    // cout<<"Enter a number:" <<endl;
-
-    // int a;
-    // taking input
-    // cin>>a;
-
-    // cout<<"You entered " <<a<<endl;
-
     int a=5;
-    cout << "Value of a is : " << a << endl;
+    int b=3;
+    // cout<<a+b<<endl;
+    // cout<<a*b<<endl;
+    // cout<<a-b<<endl;
+    // cout<<a/b<<endl;
 
-    char ch = 'a';
-    cout<<"Value of Char is : "<<ch<<endl;
+    // cout<<(5.0)/3;
+    // cout<<(a>b)<<endl;
+    // cout<<(a<b)<<endl;
+    // cout<<(a==b)<<endl;
+    // cout<<(a!=b)<<endl;
+    // cout<<(a>=b)<<endl;
+    // cout<<(a<=b)<<endl;
+// } 
+
+    // if (a>=5 && b<=3)
+    // if (a<5 || b>=3)
+    // cout<<(a<5)<<endl;
+    // cout<<!(a<5)<<endl;
+        // cout<<"Limits reached"<<endl;
 
 }
