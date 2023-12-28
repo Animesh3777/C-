63ebbc0f807733f5dcc6b0971789819ea7db7546
Animesh Kumar <animesh.ak19@gmail.com>
2023-12-28 23:16:02 +0530

    Create tempCodeRunnerFile.cpp

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+t main(){
+    int age;
+    cout<<"Enter your age"<<endl;
+    cin>>age;
+    if (age<18){
+        cout<<"You can't vote"<<endl;
+    }
+    else{
+        cout<<"You can vote"<<endl;
+    }
+}
