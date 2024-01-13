8af9b051aa2824253255c049abe0d2494a8eff8d
Animesh Kumar <animesh.ak19@gmail.com>
2024-01-13 18:49:11 +0530

    Update PatternsBasic_1.cpp

--- Patterns_Solving/PatternsBasic_1.cpp
+++ Patterns_Solving/PatternsBasic_1.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 using namespace std;
 int main(){
-    for (int row =0; row<3; row+=1){
-        for(int col=0; col<5;col+=1){
-            cout<<"*";
+    // for (int row =0; row<3; row+=1){
+    //     for(int col=0; col<5;col+=1){
+    //         cout<<"* ";
+    //     }
+    // cout<<endl;
+    // }
+
+
+    int row;
+    int col;
+    cout<<"Enter Row"<<"and Enter Columns"<<endl;
+    cin>>row;
+    cin>>col;
+    for (int i=0;i<row;i=i+1){
+        for (int j=0; j<col; j=j+1){
+            cout<<"* ";
         }
     cout<<endl;
     }
