6c2e86dcc7c08af7c5dd735cf795df3c0bdbaf3f
Animesh Kumar <animesh.ak19@gmail.com>
2024-01-15 00:15:53 +0530

    Update PatternsBasic_1.cpp

--- Patterns_Solving/PatternsBasic_1.cpp
+++ Patterns_Solving/PatternsBasic_1.cpp
@@ -9,15 +9,102 @@ int main(){
     // }
 
 
-    int row;
-    int col;
-    cout<<"Enter Row"<<"and Enter Columns"<<endl;
-    cin>>row;
-    cin>>col;
-    for (int i=0;i<row;i=i+1){
-        for (int j=0; j<col; j=j+1){
-            cout<<"* ";
+    // int row;
+    // int col;
+    // cout<<"Enter Row"<<"and Enter Columns"<<endl;
+    // cin>>row;
+    // cin>>col;
+    // for (int i=0;i<row;i=i+1){
+    //     for (int j=0; j<col; j=j+1){
+    //         cout<<"* ";
+    //     }
+    // cout<<endl;
+    // }
+
+    // for (int row=0; row<3;row=row+1){
+    //     if (row==0 || row==2){
+    //         for (int col=0;col<5;col=col+1){
+    //             cout<<"* ";
+    //         }
+    //     }
+        
+    //     else{
+    //         cout<<"* ";
+    //         for (int i=0;i<3;i=i+1){
+    //                 cout<<" ";
+    //             }
+    //             cout<<"* ";
+    //         }
+    //     cout<<endl;
+    //     }
+
+
+    // for (int i=0;i<5;i+=1){
+    //     if(i==0||i==4){
+    //         for (int j=0;j<8;j+=1){
+    //             cout<<"* ";
+    //         }
+    //     }
+    //     else{
+    //         cout<<"* ";
+    //         for (int k=0;k<6;k+=1){
+    //             cout<<"  ";
+    //         }
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // int row;
+    // int col;
+    // cout<<"Enter Row"<<endl;
+    // cout<<"Enter Columns"<<endl;
+    // cin>>row>>col;
+    // for (int i=0; i<row; i+=1){
+    //     if (i==0 || i==row-1){
+    //         for( int cols=0; cols<col; cols+=1){
+    //             cout<<"* ";
+    //         }
+    //     }
+    //     else{
+    //         cout<<"* ";
+    //         for (int k=0; k<col-2; k=k+1){
+    //             cout<<"  ";
+    //         }
+    //         cout<<"* ";
+    //     }cout<<endl;
+    // } 
+
+
+    // int n;
+    // cout<<"Enter n values : "<<endl;
+    // cin>>n;
+    // for ( int row=0; row<n; row+=1){
+    //     for (int cols=0; cols<row+1; cols+=1){
+    //         cout<<"*";
+    // }
+    // cout<<endl;
+    // }
+
+    // int n;
+    // cout<<"Enter n values : "<<endl;
+    // cin>>n;
+    // for (int row=0; row<n; row+=1){
+    //     for (int col =0; col<n-row; col=col+1){
+    //         cout<<"*";
+    //     }cout<<endl;
+    // }
+
+    int n;
+    cout<<"Enter n values : "<<endl;
+    cin>>n;
+    for (int row=0; row<=n; row+=1){
+        for (int col=0; col<row+1; col+=1){
+            cout<<col+1;
         }
-    cout<<endl;
+        cout<<endl;
     }
-}
+
+    } 
+
+
